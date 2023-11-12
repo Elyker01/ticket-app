b902e0f66ec16cc2b6b528debce0c1c7ce1a5a0a
Adelekan <elykerz@gmail.com>
2023-11-12 16:09:53 +0000

    chore: "Added background and basic ticket layout"

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,14 @@
+import PriorityDisplay from "./(components)/PriorityDisplay";
+import ProgressDisplay from "./(components)/ProgressBar";
+import StatusDisplay from "./(components)/StatusDisplay";
+import TicketCard from "./(components)/TicketCard";
+import DeleteBlock from "./(components)/deleteBlock";
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className="flex flex-col bg-card hover:bg-card-hover rounded-md shadow-lg p-3 m-2">
+      <TicketCard />
+    </div>
+  );
 };
 
 export default Dashboard;
